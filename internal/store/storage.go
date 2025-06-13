10ce48d6a82752e5f6a9acafd7e606e236c1964e
arminfatahi <arminfatahi2002@gmail.com>
2025-06-13 16:25:28 +0330

    added roles and users

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -14,10 +14,25 @@ var (
 )
 
 type Storage struct {
+	Users interface {
+		Create(context.Context, *sql.Tx, *User) error
+		GetByID(context.Context, int64) (*User, error)
+		GetByEmail(context.Context, string) (*User, error)
+		CreateAndInvite(ctx context.Context, user *User, token string, exp time.Duration) error
+		Activate(context.Context, string) error
+		Delete(context.Context, int64) error
+	}
+
+	Roles interface {
+		GetByName(context.Context, string) (*Role, error)
+	}
 }
 
 func NewPostgresStorage(db *sql.DB) Storage {
-	return Storage{}
+	return Storage{
+		Users: &UserStore{db},
+		Roles: &RoleStore{db},
+	}
 }
 
 func withTx(db *sql.DB, ctx context.Context, fn func(*sql.Tx) error) error {
